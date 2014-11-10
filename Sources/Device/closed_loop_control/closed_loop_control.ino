9732654c54af0575f7d1885f5b13091f5e41b5f6
Alexandre Brand <brand@ece.fr>
2014-11-10 15:02:19 +0100

    Added : primary version of autopilot
    - Added primary version of Autopilot (code basis)

---
+++ Sources/Device/closed_loop_control/closed_loop_control.ino
@@ -0,0 +1,27 @@
+/*  /!\Arduino 
+	CLOSED LOOP CONTROL MAIN PROGRAM : Eq. Autopilot
+	For Hovercraft using 4 lateral motors
+*/
+
+#include <Arduino.h>
+
+// Perso libraries
+#include "network_manager.h"
+#include "pid_manager.h"
+#include "scheduler.h"
+#include "motor_handler.h"
+
+// Driver library
+#include "ADNS2610_arduino_driver.h"
+
+// Particular #Define
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
