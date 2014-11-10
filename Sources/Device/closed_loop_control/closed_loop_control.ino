ac35b582ba8a8a8a7a97fe217d690a5c2a92bb61
Alexandre Brand <brand@ece.fr>
2014-11-10 17:36:07 +0100

    update autopilot

--- Sources/Device/closed_loop_control/closed_loop_control.ino
+++ Sources/Device/closed_loop_control/closed_loop_control.ino
@@ -9,19 +9,31 @@
 #include "network_manager.h"
 #include "pid_manager.h"
 #include "scheduler.h"
+
+// TODO : implementation
+// Done : 45 %
 #include "motor_handler.h"
+// Done : 80%
+#include "acquisition_handler.h"
 
-// Driver library
-#include "ADNS2610_arduino_driver.h"
-
-// Particular #Define
-
+// Particular #Defines
+// TODO : setups
 void setup()
 {
-
+	// Init Network
+	// Init motors
+	// Init PIDs
+	// Init ACQ system
+	// Init Scheduler
+	// ping end of setup
 }
 
+// TODO : levels of update (motors, ACQ, etc...)
 void loop()
 {
-
+	// Up network
+	// Up ACQ
+	// If 1/5 : run PIDs
+	// Up motors
+	// Up scheduler
 }
