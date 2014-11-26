4e64be78086bd3be26c600d1b00f8c8a7acadae4
D3Rnatch <dernatch@gmail.com>
2014-11-26 19:51:25 +0100

    Protocol Helper : Update frame extraction and encap. TODO : Special frame encap

--- Sources/Device/NET_manager/protocol_helper.cpp
+++ Sources/Device/NET_manager/protocol_helper.cpp
@@ -51,3 +51,66 @@ t_encap encap_battery_data(byte b1, byte b2)
 	
 	return t;
 }
+
+/**
+*	\brief extract_data, stores every bytes into an encap type variable
+*	\param frame : array of 5 bytes aka a frame.
+*/
+t_recept_encap extract_data(byte*frame)
+{
+	// STEP 1 : extraction de l'ID
+	uint8_t frame_type = (uint8_t)frame[0];
+	uint8_t elem = 0;
+	void * array = NULL;
+	switch(frame_type)
+	{
+		case 0 : // State and Start frame !
+			// Typages et initialisations 
+			array = new uint8_t[2]();
+			array[0] = (uint8_t)frame[1]; // Start 
+			array[0] = (uint8_t)frame[2]; // State
+			elem = 2;
+		break;
+		case 1 : // Stop signal frame
+			array = (*uint8_t)frame[1];
+			elem = 1;
+		break;
+		case 2 : // Move frame
+			array = new uint8_t[5]();
+			for(uint8_t i=0;i<5;i++)
+				array[i] = (uint8_t)frame[i];
+			elem = 5;
+		break;
+		case 3 : // Power frame
+			array = new uint8_t[5]();
+			for(uint8_t i=0;i<5;i++)
+				array[i] = (uint8_t)frame[i];
+			elem = 5;
+		break;
+		case 4 : // Point frame
+			uint16_t t = frame[0] << 8;
+			t = t | frame[1];
+			array = new double[2]();
+			array[0] = (double)t;
+			t = frame[2] << 8; 
+			t |= frame[3];
+			array[1] = (double)t;
+			elem = 2;
+		break;
+		default : // Error frame
+			uint8_t id = -1;
+			array = NULL;
+			elem = -1;
+		break;
+	}
+	t_recept_encap encap;
+	encap.type = frame_type;
+	encap.Elements = elem;
+	encap.data = array;	
+}
+
+
+
+
+
+
--- Sources/Device/NET_manager/protocol_helper.h
+++ Sources/Device/NET_manager/protocol_helper.h
@@ -2,17 +2,40 @@
 *									*
 *				PROTOCOL HELPER				*
 *									*
-**/								      \**
+**/								    //\**
 
 #ifndef PROTOCOL_HELPER_H
 #define PROTOCOL_HELPER_H
 
+/*
+	Frame structure :
+	ID|- |- |- |- |- |
+  byte :0 |1 |2 |3 |4 |5 |	
+*/
+
+/**
+*	Send/Extract : Encap structur
+*
+*/
 typedef struct send_encap
 {
 	byte array[5];
 }t_encap;
 
+typedef struct recept_encap
+{
+	uint8_t type; // Frame ID and type
+	void * data; // Elements Array
+	uint8_t Elements; // Number elements
+}t_recept_encap;
 
+// ========================================================================
+//		SENDING PROTOCOL HELPER
+//	Features :
+//		-> Ready Frame
+//		-> Data Frame
+//		-> Battery Status Frame 
+// ========================================================================
 /**
 *	\brief encap_acq_data, encaps the acquisition data in format
 *
@@ -25,10 +48,50 @@ t_encap encap_acq_data(byte, byte, byte);
 */
 t_encap encap_ready();
 
+
 /**
 *	\brief encap_battery_data, encaps the battery data in format
 *
 */
 t_encap encap_battery_data(byte, byte);
+
+// ========================================================================
+//		ENCAPSULATION PROTOCOL HELPER (Rasp side)
+//	Features :
+//		-> State & Start Frame id : 0
+//		-> Stop Frame : id 1
+//		-> Move Frame : id 2
+//		-> Power Frame : id 3
+//		-> Point Frame : id 4
+//	/!\ : Frame size = 5 bytes
+// ========================================================================
+
+/**
+*	\brief encap_special_frames : encaps raspberry side frames
+*	\param uint8_t : frame type wanted
+*	\param byte *  : 
+* 	################################### TODO
+*/
+t_encap encap_special_frames(uint8_t, byte*, uint8_t);
+
+// ========================================================================
+//		EXTRACTION PROTOCOL HELPER
+//	Features :
+//		-> State & Start Frame id : 0
+//		-> Stop Frame : id 1
+//		-> Move Frame : id 2
+//		-> Power Frame : id 3
+//		-> Point Frame : id 4
+//	/!\ : Frame size = 5 bytes
+// ========================================================================
+
+/**
+*	\brief extract_data, stores every bytes into an encap type variable
+*	\param frame : array of 5 bytes aka a frame.
+*/
+t_recept_encap extract_data(byte*frame);
+
+
+
 
 #endif
