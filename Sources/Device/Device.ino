8c6242325c3b783affa9f7a7e8c3328ea59379f2
Loïc Dalloz <osblouf@gmail.com>
2015-01-22 11:29:16 +0100

    Add device.ino

--- Sources/Device/Device.ino
+++ Sources/Device/Device.ino
@@ -23,7 +23,7 @@ void setup()
 {
         controller = new Controller();
 	controller->init();
-        Serial.println("END OF SETUP. GOING MAIN LOOP");
+        //Serial.println("END OF SETUP. GOING MAIN LOOP");
 }
 
 /**
@@ -41,19 +41,19 @@ void loop()
 	// Processing Communication
 	controller->Process_Com(id, b);	
 	
-        Serial.print("Attention : ");
-        Serial.println(id,DEC);
+        //Serial.print("Attention : ");
+        //Serial.println(id,DEC);
 
 	// If controllerState is : 
 	if(controller->controllerState == Manual_Acquisition) {
-                Serial.println("MANUAL_ACQ MODE");
+                //Serial.println("MANUAL_ACQ MODE");
 		controller->Process_Acq();
 		if(id == 3)
 			controller->Process_Motor(b);
 	}
 
 	if(controller->controllerState == Manual) {
-                Serial.println("MANUAL_ACQ MODE");
+                //Serial.println("MANUAL_ACQ MODE");
 		if(id == 3)
 			controller->Process_Motor(b);
 	}
@@ -67,7 +67,7 @@ void loop()
 	}
 
 	if(controller->controllerState == Idle) {
-                Serial.println("Idle State.");
+                //Serial.println("Idle State.");
 		// TO BE IMPLEMENTED
 	}
 }
